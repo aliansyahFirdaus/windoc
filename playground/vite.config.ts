8192dbc09171dddc4061e459f3f93af9e7f3ff38
aliansyahFirdaus <aliansyah.dev@gmail.com>
2026-03-10 13:47:50 +0700

    chore(playground): revert vite alias, use tsup watch instead

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,15 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@windoc/core/style.css': path.resolve(__dirname, '../core/dist/index.css'),
-      '@windoc/react/style.css': path.resolve(__dirname, '../react/src/styles/editor.css'),
-      '@windoc/core': path.resolve(__dirname, '../core/src/index.ts'),
-      '@windoc/react': path.resolve(__dirname, '../react/src/index.ts'),
-    },
-  },
 })
