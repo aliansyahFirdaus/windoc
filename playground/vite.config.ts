953fea8e983a046abf9ad73d773e0f5e47870f69
aliansyahFirdaus <aliansyah.dev@gmail.com>
2026-03-10 13:44:22 +0700

    chore(playground): alias style.css to dist, js to source

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -6,6 +6,8 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      '@windoc/core/style.css': path.resolve(__dirname, '../core/dist/index.css'),
+      '@windoc/react/style.css': path.resolve(__dirname, '../react/dist/index.css'),
       '@windoc/core': path.resolve(__dirname, '../core/src/index.ts'),
       '@windoc/react': path.resolve(__dirname, '../react/src/index.ts'),
     },
